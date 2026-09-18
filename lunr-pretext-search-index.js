7c9f71ef168c52180a6d595c221fda09084d8df8
Drew Lewis <drew.lewis@gmail.com>
2026-09-18 21:41:09 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,7 +16,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Real Numbers",
-  "body": " Real Numbers   What is a \"number\"?    Things we think of as \"numbers\" seem to have the following properties:   In particular, these operations should both be commutative and associative, and obey distributive rules.    There are operations and that behave as we expect.  There are identity elements: for addition and for multiplication.  These operations have inverse operations and , respectively.  Every number has an additive inverse and every non-zero number has a multiplicative inverse.   Mathematicians call anything with these properties a field .    The set of rational numbers, denoted , is a field.    The set of constructible numbers are the lengths that can be constructed with a compass and straight edge, given a line segment of length . The number is constructible, but is not.   A classical geometry question asks if, given a cube, another cube of twice the volume can be constructed with a compass and a straight edge. Showing that this is impossible is equivalent to showing that is not constructible and took over 2000 years to solve.   The set of constructible numbers also forms a field.    The set of rational functions, i.e. functions of the form , where and are polynomials, is a field.    The rational numbers (and the constructible numbers) are an ordered field : we have an inequality operator such that for any two distinct rational numbers and , either or .     Previously we asked the question: Does ?  How can we interpret as a number?      Informally, sequence (of numbers) is an infinite list of numbers.  Formally, a rational sequence is a function from the natural numbers to the rational numbers.      is a (rational) sequence. It represents the function from to given by   Often we use a notation such as for a sequence. In this example, we would further write , or perhaps even write .     Write the first 5 terms of each of these sequences                         How would you describe each of these sequences, particularly as becomes large?     "
+  "body": " Real Numbers   What is a \"number\"?    Things we think of as \"numbers\" seem to have the following properties:   In particular, these operations should both be commutative and associative, and obey distributive rules.    There are operations and that behave as we expect.  There are identity elements: for addition and for multiplication.  These operations have inverse operations and , respectively.  Every number has an additive inverse and every non-zero number has a multiplicative inverse.   Mathematicians call anything with these properties a field .    The set of rational numbers, denoted , is a field.    The set of constructible numbers are the lengths that can be constructed with a compass and straight edge, given a line segment of length . The number is constructible, but is not.   A classical geometry question asks if, given a cube, another cube of twice the volume can be constructed with a compass and a straight edge. Showing that this is impossible is equivalent to showing that is not constructible and took over 2000 years to solve.   The set of constructible numbers also forms a field.    The set of rational functions, i.e. functions of the form , where and are polynomials, is a field.    The rational numbers (and the constructible numbers) are an ordered field : we have an inequality operator such that for any two distinct rational numbers and , either or .     Consider the set of numbers .    Find a rational number that is an upper bound for ; that is, for every .    What is the smallest rational number that is an upper bound for ?     An ordered set has the least-upper-bound property if every nonempty subset that has an upper bound has a least upper bound.  An ordered field that has the least-upper-bound property is called complete .    The rational numbers does not have the least upper bound property as we saw in . In other words, is not complete.     There is a unique complete ordered field that contains the rational numbers.     Note that this theorem is saying two things: there exist complete ordered fields that contain the rationals, and further any two such fields are actually the same (i.e. it is unique).     The real numbers, denoted , are the unique complete ordered field containing .     You can think about \"building\" from by formally adding least upper bounds to every subset of .   "
 },
 {
   "id": "sec-reals-2",
@@ -73,13 +73,13 @@ var ptx_lunr_docs = [
   "body": " The rational numbers (and the constructible numbers) are an ordered field : we have an inequality operator such that for any two distinct rational numbers and , either or .  "
 },
 {
-  "id": "sec-reals-8",
+  "id": "activity-q-lub",
   "level": "2",
-  "url": "sec-reals.html#sec-reals-8",
-  "type": "Question",
+  "url": "sec-reals.html#activity-q-lub",
+  "type": "Activity",
   "number": "1.1.7",
   "title": "",
-  "body": "  Previously we asked the question: Does ?  How can we interpret as a number?   "
+  "body": "  Consider the set of numbers .    Find a rational number that is an upper bound for ; that is, for every .    What is the smallest rational number that is an upper bound for ?   "
 },
 {
   "id": "sec-reals-9",
@@ -88,7 +88,7 @@ var ptx_lunr_docs = [
   "type": "Definition",
   "number": "1.1.8",
   "title": "",
-  "body": "  Informally, sequence (of numbers) is an infinite list of numbers.  Formally, a rational sequence is a function from the natural numbers to the rational numbers.   "
+  "body": " An ordered set has the least-upper-bound property if every nonempty subset that has an upper bound has a least upper bound.  An ordered field that has the least-upper-bound property is called complete .  "
 },
 {
   "id": "sec-reals-10",
@@ -97,16 +97,43 @@ var ptx_lunr_docs = [
   "type": "Example",
   "number": "1.1.9",
   "title": "",
-  "body": "  is a (rational) sequence. It represents the function from to given by   Often we use a notation such as for a sequence. In this example, we would further write , or perhaps even write .  "
+  "body": " The rational numbers does not have the least upper bound property as we saw in . In other words, is not complete.  "
 },
 {
-  "id": "sec-reals-11",
+  "id": "thm-r-uniqe",
   "level": "2",
-  "url": "sec-reals.html#sec-reals-11",
-  "type": "Activity",
+  "url": "sec-reals.html#thm-r-uniqe",
+  "type": "Theorem",
   "number": "1.1.10",
   "title": "",
-  "body": "  Write the first 5 terms of each of these sequences                         How would you describe each of these sequences, particularly as becomes large?    "
+  "body": "  There is a unique complete ordered field that contains the rational numbers.   "
+},
+{
+  "id": "sec-reals-12",
+  "level": "2",
+  "url": "sec-reals.html#sec-reals-12",
+  "type": "Remark",
+  "number": "1.1.11",
+  "title": "",
+  "body": " Note that this theorem is saying two things: there exist complete ordered fields that contain the rationals, and further any two such fields are actually the same (i.e. it is unique).  "
+},
+{
+  "id": "def-reals",
+  "level": "2",
+  "url": "sec-reals.html#def-reals",
+  "type": "Definition",
+  "number": "1.1.12",
+  "title": "",
+  "body": "  The real numbers, denoted , are the unique complete ordered field containing .   "
+},
+{
+  "id": "sec-reals-14",
+  "level": "2",
+  "url": "sec-reals.html#sec-reals-14",
+  "type": "Remark",
+  "number": "1.1.13",
+  "title": "",
+  "body": " You can think about \"building\" from by formally adding least upper bounds to every subset of .  "
 }
 ]
 
